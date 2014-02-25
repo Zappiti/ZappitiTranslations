471f2c246311d5e4cb7afaf60201fd3a7d166a16
Filimindji <dh-alias@outlook.com>
2014-02-25 10:42:01 +0100

    Last modifications for v3.0 + ZappitiDynamic translations

---
+++ ZappitiDynamic/culture-en.js
@@ -0,0 +1,356 @@
+﻿var cultureEn = new Array();
+cultureEn.EmptyMenu = "This menu is empty.";
+cultureEn.SearchResultFor = "Results for: ";
+cultureEn.NoSearchResult = "No results.";
+cultureEn.ConnectionTitle = "Connecting";
+cultureEn.ErrorBadCredentialTitle = "Authentication failed:";
+cultureEn.ErrorBadCredentialMessage = "Thank you to enter a valide Zappiti username and password.";
+cultureEn.ErrorCloudNotActivatedTitle = "Zappiti Cloud is not activated:";
+cultureEn.ErrorCloudNotActivatedMessage = "Enable synchronization in Zappiti Media Center, Settings/Account, to continue.";
+cultureEn.ErrorZappitiPlayerNotActivatedTitle = "Zappiti Player is not registered:";
+cultureEn.ErrorZappitiPlayerNotActivatedMessage = "With Zappiti Media Center, make sure that you register the Token of your Zappiti Player to continue. Then, enable the synchronization in Settings/Account.";
+cultureEn.SignInTitle = "Connection";
+cultureEn.SignInEmail = "Email";
+cultureEn.SignInPassword = "Password";
+cultureEn.SignInEnter = "Enter";
+cultureEn.SignInShowKeyboard = "&nbsp;Display the virtual keyboard";
+cultureEn.SignInDemoLabel = "&nbsp;Show the demo account";
+cultureEn.SearchTitle = "What are you searching for?";
+cultureEn.SearchMovie = "Movies, TV Show";
+cultureEn.SearchAlbumPlaylist = "Album, Playlist";
+cultureEn.SearchTrack = "Audio title";
+cultureEn.SearchActor = "Actor";
+cultureEn.SearchDirector = "Director";
+cultureEn.SearchPerformer = "Artist, Singer";
+cultureEn.FileNotFound = "File not found:";
+cultureEn.HelpSilver = "help-silver-en.jpg";
+cultureEn.HelpDefault = "help-default-en.jpg";
+cultureEn.AudioHelpSilver = "audio-help-silver-en.jpg";
+cultureEn.AudioHelpDefault = "audio-help-default-en.jpg";
+cultureEn.Yes = "Yes";
+cultureEn.No = "No";
+cultureEn.OK = "OK";
+cultureEn.QuitZappitiMessage = "Do you want to quit Zappiti and display the native interface ?";
+cultureEn.ChangelogTitle = "Version 0.3";
+cultureEn.ChangelogMessage = "<div style='text-align:left'>Improvements :" +
+    "<ul>" +
+        "<li>Possibility of changing the seen/unseen state with POP UP MENU button of the remote.</li>" +
+        "<li>NB : This requiere Zappiti v3.0 installed on your computer :" +
+        "<li>http://bit.ly/zmc-3-0-preview-en</li>" +
+    "</ul></div>";
+cultureEn.SetSeenstateTitle = "Visualisation state";
+cultureEn.Unseen = "Unseen";
+cultureEn.Seen = "Seen";
+cultureEn.Ongoing = "Ongoing";
+
+cultureEn.Keyboard = {
+    // la liste de panels
+    'alpha': [
+        [
+            { "key": "q" },
+            { "key": "w" },
+            { "key": "e" },
+            { "key": "r" },
+            { "key": "t" },
+            { "key": "y" },
+            { "key": "u" },
+            { "key": "i" },
+            { "key": "o" },
+            { "key": "p" }
+        ],
+        [
+            { "key": "a" },
+            { "key": "s" },
+            { "key": "d" },
+            { "key": "f" },
+            { "key": "g" },
+            { "key": "h" },
+            { "key": "j" },
+            { "key": "k" },
+            { "key": "l" },
+            { "key": "!" }
+        ],
+        [
+            { "key": "z" },
+            { "key": "x" },
+            { "key": "c" },
+            { "key": "v" },
+            { "key": "b" },
+            { "key": "n" },
+            { "key": "m" },
+            { "key": "," },
+            { "key": "." },
+            { "key": "?" }
+        ],
+        [
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric-up', 'class': 'dark' },
+            { "key": " ", "width": 6, 'caption': 'Space' },
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric-up', 'class': 'dark' }
+        ]
+    ],
+
+    'alpha-up': [
+        [
+            { "key": "Q" },
+            { "key": "W" },
+            { "key": "E" },
+            { "key": "R" },
+            { "key": "T" },
+            { "key": "Y" },
+            { "key": "U" },
+            { "key": "I" },
+            { "key": "O" },
+            { "key": "P" }
+        ],
+        [
+            { "key": "A" },
+            { "key": "S" },
+            { "key": "D" },
+            { "key": "F" },
+            { "key": "G" },
+            { "key": "H" },
+            { "key": "J" },
+            { "key": "K" },
+            { "key": "L" },
+            { "key": "-" }
+        ],
+        [
+            { "key": "Z" },
+            { "key": "X" },
+            { "key": "C" },
+            { "key": "V" },
+            { "key": "B" },
+            { "key": "N" },
+            { "key": "M" },
+            { "key": ";" },
+            { "key": ":" },
+            { "key": "/" },
+        ],
+        [
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric', 'class': 'dark pressed' },
+            { "key": " ", "width": 6, 'caption': 'Space' },
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric', 'class': 'dark pressed' }
+        ]
+    ],
+
+    'symbol': [
+        [
+            { "key": "&acirc;" },
+            { "key": "&agrave;" },
+            { "key": "&auml;" },
+            { "key": "&aelig;" },
+            { "key": "&eacute;" },
+            { "key": "&egrave;" },
+            { "key": "&ecirc;" },
+            { "key": "&euml;" },
+            { "key": "-" },
+            { "key": "+" }
+        ],
+        [
+            { "key": "&icirc;" },
+            { "key": "&iuml;" },
+            { "key": "&ocirc;" },
+            { "key": "&ouml;" },
+            { "key": "&ugrave;" },
+            { "key": "&ucirc;" },
+            { "key": "&uuml;" },
+            { "key": "&ccedil;" },
+            { "key": "*" },
+            { "key": ";" }
+        ],
+        [
+            { "key": "&euro;" },
+            { "key": "$" },
+            { "key": "&pound;" },
+            { "key": "&lt;" },
+            { "key": "&gt;" },
+            { "key": "(" },
+            { "key": ")" },
+            { "key": "[" },
+            { "key": "]" },
+            { "key": "@" }
+        ],
+        [
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols-up', 'class': 'dark' },
+            { "key": " ", "width": 6, 'caption': 'Space' },
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols-up', 'class': 'dark' }
+        ]
+    ],
+
+    'symbol-up': [
+        [
+            { "key": "&Acirc;" },
+            { "key": "&Agrave;" },
+            { "key": "&Auml;" },
+            { "key": "&AElig;" },
+            { "key": "&Eacute;" },
+            { "key": "&Egrave;" },
+            { "key": "&Ecirc;" },
+            { "key": "&Euml;" },
+            { "key": "-" },
+            { "key": "+" }
+        ],
+        [
+            { "key": "&Icirc;" },
+            { "key": "&Iuml;" },
+            { "key": "&Ocirc;" },
+            { "key": "&Ouml;" },
+            { "key": "&Ugrave;" },
+            { "key": "&Ucirc;" },
+            { "key": "&Uuml;" },
+            { "key": "&Ccedil;" },
+            { "key": "*" },
+            { "key": ";" }
+        ],
+        [
+            { "key": "&yen;" },
+            { "key": "&sect;" },
+            { "key": "&deg;" },
+            { "key": "`" },
+            { "key": "&sup2;" },
+            { "key": "{" },
+            { "key": "}" },
+            { "key": "^" },
+            { "key": "~" },
+            { "key": "&mu;" }
+        ],
+        [
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols', 'class': 'dark pressed' },
+            { "key": " ", "width": 6, 'caption': 'Space' },
+            { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols', 'class': 'dark pressed' }
+        ]
+    ],
+
+    'numeric': [
+        [
+            { "key": "1" },
+            { "key": "2" },
+            { "key": "3" }
+        ],
+        [
+            { "key": "4" },
+            { "key": "5" },
+            { "key": "6" }
+        ],
+        [
+            { "key": "7" },
+            { "key": "8" },
+            { "key": "9" }
+        ],
+        [
+            { "caption": "&lArr;", 'class': 'dark' },
+            { "key": "0" },
+            { "caption": "&rArr;", 'class': 'dark' }
+        ]
+    ],
+
+
+    'symbol2': [
+        [
+            { "key": "\\" },
+            { "key": "#" },
+            { "key": "=" }
+        ],
+        [
+            { "key": "." },
+            { "key": "|" },
+            { "key": "_" }
+        ],
+        [
+            { "key": "&amp;" },
+            { "key": "&quot;" },
+            { "key": "'" }
+        ],
+        [
+            { "caption": "&lArr;", 'class': 'dark' },
+            { "key": "%" },
+            { "caption": "&rArr;", 'class': 'dark' }
+        ]
+    ],
+
+
+    'control': [
+        [
+            { "caption": "&#x232b;", 'cmdErase': 1 }
+        ],
+        [
+            { "caption": "Clear", "cmdClear": 1 }
+        ],
+        [
+            { "caption": "Symbols", 'cmdShow': 'symbols' }
+        ],
+        [
+            { "caption": "Enter", 'cmdSubmit': 1 }
+        ]
+    ],
+
+    'control-up': [
+        [
+            { "caption": "&#x232b;", 'cmdErase': 1 }
+        ],
+        [
+            { "caption": "Clear", "cmdClear": 1 }
+        ],
+        [
+            { "caption": "Symbols", 'cmdShow': 'symbols-up' }
+        ],
+        [
+            { "caption": "Enter", 'cmdSubmit': 1 }
+        ]
+    ],
+
+    'control2': [
+        [
+            { "caption": "&#x232b;", 'cmdErase': 1 }
+        ],
+        [
+            { "caption": "Clear", "cmdClear": 1 }
+        ],
+        [
+            { "caption": "abc", 'cmdShow': 'alpha-numeric' }
+        ],
+        [
+            { "caption": "Enter", 'cmdSubmit': 1 }
+        ]
+    ],
+
+    'control2-up': [
+        [
+            { "caption": "&#x232b;", 'cmdErase': 1 }
+        ],
+        [
+            { 'caption': "Clear", "cmdClear": 1 }
+        ],
+        [
+            { "caption": "abc", 'cmdShow': 'alpha-numeric-up' }
+        ],
+        [
+            { "caption": "Enter", 'cmdSubmit': 1 }
+        ]
+    ],
+
+    // la liste des sets
+    'alpha-numeric': [
+            { 'name': 'alpha', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+            { 'name': 'control', 'width': "200px", "padding": "0px 16px 0px 16px" },
+            { 'name': 'numeric', 'width': "355px", "padding": "0px 0px 0px 0px" }
+    ],
+
+    'alpha-numeric-up': [
+            { 'name': 'alpha-up', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+            { 'name': 'control-up', 'width': "200px", "padding": "0px 16px 0px 16px" },
+            { 'name': 'numeric', 'width': "355px", "padding": "0px 0px 0px 0px" }
+    ],
+
+    'symbols': [
+            { 'name': 'symbol', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+            { 'name': 'control2', 'width': "200px", "padding": "0px 16px 0px 16px" },
+            { 'name': 'symbol2', 'width': "355px", "padding": "0px 0px 0px 0px" }
+    ],
+
+    'symbols-up': [
+            { 'name': 'symbol-up', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+            { 'name': 'control2-up', 'width': "200px", "padding": "0px 16px 0px 16px" },
+            { 'name': 'symbol2', 'width': "355px", "padding": "0px 0px 0px 0px" }
+    ]
+};
---
+++ ZappitiDynamic/culture-fr.js
@@ -0,0 +1,356 @@
+﻿var cultureFr = new Array();
+cultureFr.EmptyMenu = "Ce menu est vide.";
+cultureFr.SearchResultFor = "R&eacute;sultats pour : ";
+cultureFr.NoSearchResult = "Aucun r&eacute;sultat trouv&eacute;.";
+cultureFr.ConnectionTitle = "Connexion en cours";
+cultureFr.ErrorBadCredentialTitle = "L'authentification a &eacute;chou&eacute;e :";
+cultureFr.ErrorBadCredentialMessage = "Merci de saisir un identifiant Zappiti et un mot de passe valide.";
+cultureFr.ErrorCloudNotActivatedTitle = "Zappiti Cloud n'est pas activ&eacute; :";
+cultureFr.ErrorCloudNotActivatedMessage = "Activez la synchronisation dans Zappiti Media Center, Param&egrave;tres/Compte pour continuer.";
+cultureFr.ErrorZappitiPlayerNotActivatedTitle = "Zappiti Player n'est pas activ&eacute; :";
+cultureFr.ErrorZappitiPlayerNotActivatedMessage = "Avec Zappiti Media Center, assurez vous d'avoir enregistr&eacute; le Token de votre Zappiti Player pour continuer. Puis, activez la synchronisation dans Param&egrave;tres/Compte.";
+cultureFr.SignInTitle = "Connexion";
+cultureFr.SignInEmail = "Email";
+cultureFr.SignInPassword = "Mot de passe";
+cultureFr.SignInEnter = "Enter";
+cultureFr.SignInShowKeyboard = "&nbsp;Afficher le clavier virtuel";
+cultureFr.SignInDemoLabel = "&nbsp;Voir le compte de d&eacute;monstration";
+cultureFr.SearchTitle = "Que cherchez vous ?";
+cultureFr.SearchMovie = "Film, S&eacute;rie";
+cultureFr.SearchAlbumPlaylist = "Album, Playlist";
+cultureFr.SearchTrack = "Titre audio";
+cultureFr.SearchActor = "Acteur";
+cultureFr.SearchDirector = "R&eacute;alisateur";
+cultureFr.SearchPerformer = "Artiste, Intertr&egravete";
+cultureFr.FileNotFound = "Fichier non trouv&eacute; :";
+cultureFr.HelpSilver = "help-silver-fr.jpg";
+cultureFr.HelpDefault = "help-default-fr.jpg";
+cultureFr.AudioHelpSilver = "audio-help-silver-fr.jpg";
+cultureFr.AudioHelpDefault = "audio-help-default-fr.jpg";
+cultureFr.Yes = "Oui";
+cultureFr.No = "Non";
+cultureFr.OK = "OK";
+cultureFr.QuitZappitiMessage = "Voulez vous quitter Zappiti et revenir &agrave; l'interface native ?";
+cultureFr.ChangelogTitle = "Version 0.3";
+cultureFr.ChangelogMessage = "<div style='text-align:left'>Am&eacuteliorations :" +
+    "<ul>" +
+        "<li>Possibilit&eacute; de changer l'&eacute;tat vu/non vu avec la touche POP UP MENU de la t&eacute;l&eacute;commande.</li>" +
+        "<li>Nb : Necessite Zappiti v3.0 sur votre ordinateur :" +
+        "<li>http://bit.ly/zmc-3-0-preview-fr</li>" +
+    "</ul></div>";
+cultureFr.SetSeenstateTitle = "Etat de la visualisation";
+cultureFr.Unseen = "Non vu";
+cultureFr.Seen = "Vu";
+cultureFr.Ongoing = "En cours";
+
+cultureFr.Keyboard = {
+            // la liste de panels
+            'alpha': [
+                [
+                    { "key": "a" },
+                    { "key": "z" },
+                    { "key": "e" },
+                    { "key": "r" },
+                    { "key": "t" },
+                    { "key": "y" },
+                    { "key": "u" },
+                    { "key": "i" },
+                    { "key": "o" },
+                    { "key": "p" }
+                ],
+                [
+                    { "key": "q" },
+                    { "key": "s" },
+                    { "key": "d" },
+                    { "key": "f" },
+                    { "key": "g" },
+                    { "key": "h" },
+                    { "key": "j" },
+                    { "key": "k" },
+                    { "key": "l" },
+                    { "key": "m" }
+                ],
+                [
+                    { "key": "w" },
+                    { "key": "x" },
+                    { "key": "c" },
+                    { "key": "v" },
+                    { "key": "b" },
+                    { "key": "n" },
+                    { "key": "," },
+                    { "key": "." },
+                    { "key": "?" },
+                    { "key": "!" }
+                ],
+                [
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric-up', 'class': 'dark' },
+                    { "key": " ", "width": 6, 'caption': 'Espace' },
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric-up', 'class': 'dark' }
+                ]
+            ],
+
+            'alpha-up': [
+                [
+                    { "key": "A" },
+                    { "key": "Z" },
+                    { "key": "E" },
+                    { "key": "R" },
+                    { "key": "T" },
+                    { "key": "Y" },
+                    { "key": "U" },
+                    { "key": "I" },
+                    { "key": "O" },
+                    { "key": "P" }
+                ],
+                [
+                    { "key": "Q" },
+                    { "key": "S" },
+                    { "key": "D" },
+                    { "key": "F" },
+                    { "key": "G" },
+                    { "key": "H" },
+                    { "key": "J" },
+                    { "key": "K" },
+                    { "key": "L" },
+                    { "key": "M" }
+                ],
+                [
+                    { "key": "W" },
+                    { "key": "X" },
+                    { "key": "C" },
+                    { "key": "V" },
+                    { "key": "B" },
+                    { "key": "N" },
+                    { "key": ";" },
+                    { "key": ":" },
+                    { "key": "/" },
+                    { "key": "-" }
+                ],
+                [
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric', 'class': 'dark pressed' },
+                    { "key": " ", "width": 6, 'caption': 'Espace' },
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'alpha-numeric', 'class': 'dark pressed' }
+                ]
+            ],
+
+            'symbol': [
+                [
+                    { "key": "&acirc;" },
+                    { "key": "&agrave;" },
+                    { "key": "&auml;" },
+                    { "key": "&aelig;" },
+                    { "key": "&eacute;" },
+                    { "key": "&egrave;" },
+                    { "key": "&ecirc;" },
+                    { "key": "&euml;" },
+                    { "key": "-" },
+                    { "key": "+" }
+                ],
+                [
+                    { "key": "&icirc;" },
+                    { "key": "&iuml;" },
+                    { "key": "&ocirc;" },
+                    { "key": "&ouml;" },
+                    { "key": "&ugrave;" },
+                    { "key": "&ucirc;" },
+                    { "key": "&uuml;" },
+                    { "key": "&ccedil;" },
+                    { "key": "*" },
+                    { "key": ";" }
+                ],
+                [
+                    { "key": "&euro;" },
+                    { "key": "$" },
+                    { "key": "&pound;" },
+                    { "key": "&lt;" },
+                    { "key": "&gt;" },
+                    { "key": "(" },
+                    { "key": ")" },
+                    { "key": "[" },
+                    { "key": "]" },
+                    { "key": "@" }
+                ],
+                [
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols-up', 'class': 'dark' },
+                    { "key": " ", "width": 6, 'caption': 'Espace' },
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols-up', 'class': 'dark' }
+                ]
+            ],
+
+            'symbol-up': [
+                [
+                    { "key": "&Acirc;" },
+                    { "key": "&Agrave;" },
+                    { "key": "&Auml;" },
+                    { "key": "&AElig;" },
+                    { "key": "&Eacute;" },
+                    { "key": "&Egrave;" },
+                    { "key": "&Ecirc;" },
+                    { "key": "&Euml;" },
+                    { "key": "-" },
+                    { "key": "+" }
+                ],
+                [
+                    { "key": "&Icirc;" },
+                    { "key": "&Iuml;" },
+                    { "key": "&Ocirc;" },
+                    { "key": "&Ouml;" },
+                    { "key": "&Ugrave;" },
+                    { "key": "&Ucirc;" },
+                    { "key": "&Uuml;" },
+                    { "key": "&Ccedil;" },
+                    { "key": "*" },
+                    { "key": ";" }
+                ],
+                [
+                    { "key": "&yen;" },
+                    { "key": "&sect;" },
+                    { "key": "&deg;" },
+                    { "key": "`" },
+                    { "key": "&sup2;" },
+                    { "key": "{" },
+                    { "key": "}" },
+                    { "key": "^" },
+                    { "key": "~" },
+                    { "key": "&mu;" }
+                ],
+                [
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols', 'class': 'dark pressed' },
+                    { "key": " ", "width": 6, 'caption': 'Espace' },
+                    { "caption": "&#x21e7;", "width": 2, 'cmdShow': 'symbols', 'class': 'dark pressed' }
+                ]
+            ],
+
+            'numeric': [
+                [
+                    { "key": "1" },
+                    { "key": "2" },
+                    { "key": "3" }
+                ],
+                [
+                    { "key": "4" },
+                    { "key": "5" },
+                    { "key": "6" }
+                ],
+                [
+                    { "key": "7" },
+                    { "key": "8" },
+                    { "key": "9" }
+                ],
+                [
+                    { "caption": "&lArr;", 'class': 'dark' },
+                    { "key": "0" },
+                    { "caption": "&rArr;", 'class': 'dark' }
+                ]
+            ],
+
+
+            'symbol2': [
+                [
+                    { "key": "\\" },
+                    { "key": "#" },
+                    { "key": "=" }
+                ],
+                [
+                    { "key": "." },
+                    { "key": "|" },
+                    { "key": "_" }
+                ],
+                [
+                    { "key": "&amp;" },
+                    { "key": "&quot;" },
+                    { "key": "'" }
+                ],
+                [
+                    { "caption": "&lArr;", 'class': 'dark' },
+                    { "key": "%" },
+                    { "caption": "&rArr;", 'class': 'dark' }
+                ]
+            ],
+
+
+            'control': [
+                [
+                    { "caption": "&#x232b;", 'cmdErase': 1 }
+                ],
+                [
+                    { "caption": "Effacer", "cmdClear": 1 }
+                ],
+                [
+                    { "caption": "Symboles", 'cmdShow': 'symbols' }
+                ],
+                [
+                    { "caption": "Entr&eacute;e", 'cmdSubmit': 1 }
+                ]
+            ],
+
+            'control-up': [
+                [
+                    { "caption": "&#x232b;", 'cmdErase': 1 }
+                ],
+                [
+                    { "caption": "Effacer", "cmdClear": 1 }
+                ],
+                [
+                    { "caption": "Symboles", 'cmdShow': 'symbols-up' }
+                ],
+                [
+                    { "caption": "Entr&eacute;e", 'cmdSubmit': 1 }
+                ]
+            ],
+
+            'control2': [
+                [
+                    { "caption": "&#x232b;", 'cmdErase': 1 }
+                ],
+                [
+                    { "caption": "Effacer", "cmdClear": 1 }
+                ],
+                [
+                    { "caption": "abc", 'cmdShow': 'alpha-numeric' }
+                ],
+                [
+                    { "caption": "Entr&eacute;e", 'cmdSubmit': 1 }
+                ]
+            ],
+
+            'control2-up': [
+                [
+                    { "caption": "&#x232b;", 'cmdErase': 1 }
+                ],
+                [
+                    { 'caption': "Effacer", "cmdClear": 1 }
+                ],
+                [
+                    { "caption": "abc", 'cmdShow': 'alpha-numeric-up' }
+                ],
+                [
+                    { "caption": "Entr&eacute;e", 'cmdSubmit': 1 }
+                ]
+            ],
+
+            // la liste des sets
+            'alpha-numeric': [
+                    { 'name': 'alpha', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+                    { 'name': 'control', 'width': "200px", "padding": "0px 16px 0px 16px" },
+                    { 'name': 'numeric', 'width': "355px", "padding": "0px 0px 0px 0px" }
+            ],
+
+            'alpha-numeric-up': [
+                    { 'name': 'alpha-up', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+                    { 'name': 'control-up', 'width': "200px", "padding": "0px 16px 0px 16px" },
+                    { 'name': 'numeric', 'width': "355px", "padding": "0px 0px 0px 0px" }
+            ],
+
+            'symbols': [
+                    { 'name': 'symbol', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+                    { 'name': 'control2', 'width': "200px", "padding": "0px 16px 0px 16px" },
+                    { 'name': 'symbol2', 'width': "355px", "padding": "0px 0px 0px 0px" }
+            ],
+
+            'symbols-up': [
+                    { 'name': 'symbol-up', 'width': "1160px", "padding": "0px 0px 0px 0px" },
+                    { 'name': 'control2-up', 'width': "200px", "padding": "0px 16px 0px 16px" },
+                    { 'name': 'symbol2', 'width': "355px", "padding": "0px 0px 0px 0px" }
+            ]
+};
